6008656c09b9e7dcfcae9c22c1194e135178b64d
BoLjshOJ <boljshoj@gmail.com>
2019-12-16 11:05:20 +0300

    initial commit

---
+++ src/main/java/com/boljshoj/StudentsJournal/StudentsJournalApplication.java
@@ -0,0 +1,13 @@
+package com.boljshoj.StudentsJournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsJournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boljshoj/StudentsJournal/StudentsJournalApplicationTests.java
@@ -0,0 +1,12 @@
+package com.boljshoj.StudentsJournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
