5ea9deb92aa6c7e249878737ffc22331b703208c
boljshoj <123qwepioneeraZd>
2020-01-10 23:59:40 +0300

    fix bugs with mail verification and flyway migration

--- src/main/resources/db/migration/V2__add_admin.sql
+++ src/main/resources/db/migration/V2__add_admin.sql
@@ -1,5 +1,5 @@
 insert into usr (id, username, password, is_active)
-    values (1, 'admin', '123', true);
+    values (999, 'admin', '123', true);
 
 insert into user_role (user_id, roles)
-    values (1, 'USER'), (1, 'ADMIN');
+    values (999, 'USER'), (999, 'ADMIN');
